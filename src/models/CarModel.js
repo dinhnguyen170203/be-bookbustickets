c39f1a2b749a680f494534200943d79f6a1028d4
dinhnguyen170203 <dinhnguyen170203@gmail.com>
2024-08-25 20:13:39 +0700

    create CarModel.js

---
+++ src/models/CarModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const carSchema = new mongoose.Schema(
+    {
+        name: { type: String, require: true},
+        licensePlate: { type: String, require: true},
+        numberOfSeats: { type: Number, require: true},
+        yearOfManufacture: { type: String, require: false},
+        type: { type: String, require: true},
+        color: { type: String, require: true},
+        image: { type: String, require: true},
+        status:  { 
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'StatusCar',
+            required: true
+        },
+        note: { type: String, require: false},
+    }
+)
+
+const Car = mongoose.model("Car", carSchema)
+module.exports = Car
