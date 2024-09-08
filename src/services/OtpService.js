fc5fdc1d7f239f056e91af5d67b7dae081d84f32
yudlinh0810 <yudlinh0810@gmail.com>
2024-09-08 21:56:59 +0700

    u19 add library nodemailer, otp-generator; complete OtpService

---
+++ src/services/OtpService.js
@@ -0,0 +1,41 @@
+const Otp = require('../models/OtpModel');
+const bcrypt = require('bcrypt');
+
+const insertOtp = ({ otp, email, hashPassword, name }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(otp, salt);
+
+      const createOtp = await Otp.create({
+        email,
+        otp: hash,
+        password: hashPassword,
+        name,
+      });
+
+      if (createOtp) {
+        resolve({
+          data: createOtp,
+        });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const isValidOtp = async (otp, hashOtp) => {
+  try {
+    const isValid = await bcrypt.compare(otp, hashOtp);
+
+    return isValid;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = {
+  insertOtp,
+  isValidOtp,
+};
